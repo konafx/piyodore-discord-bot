3ce9dd1668e82c058a6aa00964b489c8a2ed32fd
konafx <22848261+konafx@users.noreply.github.com>
2023-01-11 18:22:20 +0900

    feat: add botButton type and validate

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,11 @@
 import { AutocompleteInteraction, CommandInteraction } from 'discord.js';
 import { RawApplicationCommandData } from 'discord.js/typings/rawDataTypes';
 
+// https://stackoverflow.com/questions/51813272/declaring-string-type-with-min-max-length-in-typescript
+type StringOfLength<Min, Max> = string & {
+  StringOfLength: unique symbol // this is the phantom type
+};
+
 interface BotCommand {
   command: SlashCommandBuilder;
   execute: (interation: Interaction) => void;
@@ -13,6 +18,14 @@ interface BotEvent {
   name: string;
   once?: boolean | false;
   execute: (...args) => void;
+}
+
+type BotButtonCommandName = StringOfLength<1,32>
+type BotButtonCustomId = StringOfLength<1,36>
+
+interface BotButton {
+  commandName: BotButtonCommandName;
+  customId: BotButtonCustomId;
 }
 
 declare module 'discord.js' {
