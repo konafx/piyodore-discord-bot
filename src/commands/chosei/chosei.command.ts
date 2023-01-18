d425776db56ce9647e68e19735baaa693e9d1c8c
konafx <afxlieonon@gmail.com>
2023-01-18 17:43:50 +0900

    feat(tentive-event): [WIP] create modal
    ref: #21

--- src/commands/chosei/chosei.command.ts
+++ src/commands/chosei/chosei.command.ts
@@ -1,4 +1,12 @@
-import { Interaction, SlashCommandBuilder, SlashCommandSubcommandBuilder } from 'discord.js';
+import {
+  ActionRowBuilder,
+  Interaction,
+  ModalBuilder,
+  SlashCommandBuilder,
+  SlashCommandSubcommandBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+} from 'discord.js';
 import { eq, when } from '~/lib/when';
 import { BotCommand } from '~/types';
 import { ChoseiHostPage } from './pages/host.page';
@@ -10,6 +18,17 @@ export const ChoseiCommand: BotCommand = {
     .setDescription('Adjustment schedule')
     .addSubcommand((cmd: SlashCommandSubcommandBuilder) =>
       cmd.setName('host').setDescription('create event to adjust schedule')
+    )
+    .addSubcommand((cmd: SlashCommandSubcommandBuilder) =>
+      cmd
+        .setName('modal')
+        .setDescription('create event to adjust schedule')
+        .addStringOption((options) =>
+          options.setName('name').setDescription('set event name').setMaxLength(20).setRequired(false)
+        )
+        .addStringOption((options) =>
+          options.setName('detail').setDescription('set event detail').setMaxLength(100).setRequired(false)
+        )
     ),
   execute: async (interaction: Interaction) => {
     console.log(interaction);
@@ -18,6 +37,45 @@ export const ChoseiCommand: BotCommand = {
         .on(eq('host'), async () => {
           await interaction.reply(ChoseiHostPage('test', 'sample'));
           return 'host';
+        })
+        .on(eq('modal'), async () => {
+          const name = interaction.options.getString('name');
+          const detail = interaction.options.getString('detail');
+
+          if (name && detail) {
+            await interaction.reply(ChoseiHostPage(name, detail));
+          }
+
+          const modal = new ModalBuilder().setCustomId('hostForm').setTitle('Host new event');
+
+          const eventNameInput = new TextInputBuilder()
+            .setCustomId('eventNameInput')
+            .setLabel('What name of event')
+            .setRequired()
+            .setStyle(TextInputStyle.Short);
+
+          if (name) {
+            eventNameInput.setValue(name);
+          }
+
+          const eventDetailInput = new TextInputBuilder()
+            .setCustomId('eventDetailInput')
+            .setLabel('detail')
+            .setMaxLength(100)
+            .setRequired(false)
+            .setStyle(TextInputStyle.Paragraph);
+
+          if (detail) {
+            eventDetailInput.setValue(detail);
+          }
+
+          modal.addComponents(
+            new ActionRowBuilder<TextInputBuilder>().addComponents(eventNameInput),
+            new ActionRowBuilder<TextInputBuilder>().addComponents(eventDetailInput)
+          );
+
+          await interaction.showModal(modal);
+          return 'modal';
         })
         .otherwise(async () => {
           const message = `[chosei] otherwise: ${interaction.options.getSubcommand()}`;
