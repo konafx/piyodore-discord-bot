5f7c00a4f04ef7735967a24d9953ae90a99ce5b7
konafx <afxlieonon@gmail.com>
2023-01-26 11:30:45 +0900

    fix: add underscore to unused-vars on when.ts

--- src/lib/when.ts
+++ src/lib/when.ts
@@ -1,20 +1,20 @@
 type ChainedWhen<T, R> = {
-  on: <A>(pred: (v: T) => boolean, fn: () => A) => ChainedWhen<T, R | A>;
-  otherwise: <A>(fn: () => A) => R | A;
+  on: <A>(_pred: (_v: T) => boolean, _fn: () => A) => ChainedWhen<T, R | A>;
+  otherwise: <A>(_fn: () => A) => R | A;
 };
 
 const match = <T, R>(val: any): ChainedWhen<T, R> => ({
-  on: <A>(_pred: (v: T) => boolean, _fn: () => A) => match<T, R | A>(val),
+  on: <A>(_pred: (_v: T) => boolean, _fn: () => A) => match<T, R | A>(val),
   otherwise: <A>(_fn: () => A): A | R => val,
 });
 
 const chain = <T, R>(val: T): ChainedWhen<T, R> => ({
-  on: <A>(pred: (v: T) => boolean, fn: () => A) => (pred(val) ? match(fn()) : chain<T, A | R>(val)),
+  on: <A>(pred: (_v: T) => boolean, fn: () => A) => (pred(val) ? match(fn()) : chain<T, A | R>(val)),
   otherwise: <A>(fn: () => A) => fn(),
 });
 
 export const when = <T>(val: T) => ({
-  on: <A>(pred: (v: T) => boolean, fn: () => A) => (pred(val) ? match<T, A>(fn()) : chain<T, A>(val)),
+  on: <A>(pred: (_v: T) => boolean, fn: () => A) => (pred(val) ? match<T, A>(fn()) : chain<T, A>(val)),
 });
 
 export const eq =
