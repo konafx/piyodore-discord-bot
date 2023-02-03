2cfb0c2a8a0908dc228bf5b658ec21570c81dddc
konafx <22848261+konafx@users.noreply.github.com>
2023-02-04 02:19:26 +0900

    refactor(form): implement form class
    Formクラスを定義、inputの名前はインスタンス生成時にstringのUnion型を渡す想定
    ひとまず動いたのでcommit
    
    ref: #7

--- src/commands/chosei/forms/host.form.ts
+++ src/commands/chosei/forms/host.form.ts
@@ -1,88 +1,84 @@
-import { ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder, ModalSubmitInteraction } from 'discord.js';
+import { TextInputStyle, ModalSubmitInteraction } from 'discord.js';
+import { Form, ModalSubmitHandler } from '~/lib/form';
+import { createRoute, routeCommandName, routeCustomId } from '~/lib/route';
 import { connectTentativeEventToMessageIdsService } from '~/services/connectTentativeEventToMessageIds';
 import { createTentativeEventService } from '~/services/createTentativeEvent';
 import { ChoseiHostPage } from '../pages/host.page';
 
-const customId = 'hostForm';
+const route = createRoute(routeCommandName('chosei'), routeCustomId('hostForm'));
 
-type ModalProps = {
-  form: {
-    title?: string;
-  };
-  input: {
-    name?: string;
-    detail?: string;
-  };
+type Inputs = {
+  name?: string;
+  detail?: string;
 };
 
-export const getInputsChoseiHostForm = (i: ModalSubmitInteraction) => ({
+const getInputsChoseiHostForm = (i: ModalSubmitInteraction): Required<Inputs> => ({
   name: i.fields.getTextInputValue('eventNameInput'),
   detail: i.fields.getTextInputValue('eventDetailInput'),
 });
 
-export const ChoseiHostForm = {
-  modal: (props: ModalProps) => {
-    const modal = new ModalBuilder().setCustomId(customId).setTitle(props.form.title ?? 'Host new event');
+const handler: ModalSubmitHandler = async (i: ModalSubmitInteraction) => {
+  const inputs = getInputsChoseiHostForm(i);
 
-    const eventNameInput = new TextInputBuilder()
-      .setCustomId('eventNameInput')
-      .setLabel('What name of event')
-      .setRequired()
-      .setStyle(TextInputStyle.Short);
-
-    if (props.input.name) {
-      eventNameInput.setValue(props.input.name);
-    }
-
-    const eventDetailInput = new TextInputBuilder()
-      .setCustomId('eventDetailInput')
-      .setLabel('detail')
-      .setMaxLength(100)
-      .setRequired(false)
-      .setStyle(TextInputStyle.Paragraph);
-
-    if (props.input.detail) {
-      eventDetailInput.setValue(props.input.detail);
-    }
-
-    modal.addComponents(
-      new ActionRowBuilder<TextInputBuilder>().addComponents(eventNameInput),
-      new ActionRowBuilder<TextInputBuilder>().addComponents(eventDetailInput)
-    );
-
-    return modal;
-  },
-  handler: async (i: ModalSubmitInteraction) => {
-    const inputs = getInputsChoseiHostForm(i);
-
-    const tentativeEvent = await createTentativeEventService({
-      discordHostUserId: i.user.id,
-      tentativeEventName: inputs.name,
-    });
-
-    const m = await i.reply(ChoseiHostPage(inputs.name, inputs.detail));
-    if (!m) {
-      i.followUp({
-        content: 'something happen, failed update tentative event',
-        ephemeral: true,
-      });
-      return;
-    }
-
-    await connectTentativeEventToMessageIdsService({
-      tentativeEvent,
-      guildChannelMessageId: {
-        guildId: m.interaction.guildId as string,
-        channnelId: m.interaction.channelId as string,
-        messageId: m.id,
-        postedAt: m.interaction.createdAt,
-      },
-    });
-
+  //validate
+  if (inputs.name.length == 0) {
     i.followUp({
-      content: 'success create tentative-event',
+      content: 'Need name',
+      ephemeral: true,
+    });
+  }
+
+  const tentativeEvent = await createTentativeEventService({
+    discordHostUserId: i.user.id,
+    tentativeEventName: inputs.name,
+  });
+
+  const m = await i.reply(ChoseiHostPage(inputs.name, inputs.detail));
+  if (!m) {
+    i.followUp({
+      content: 'something happen, failed update tentative event',
       ephemeral: true,
     });
     return;
-  },
+  }
+
+  await connectTentativeEventToMessageIdsService({
+    tentativeEvent,
+    guildChannelMessageId: {
+      guildId: m.interaction.guildId as string,
+      channnelId: m.interaction.channelId as string,
+      messageId: m.id,
+      postedAt: m.interaction.createdAt,
+    },
+  });
+
+  i.followUp({
+    content: 'success create tentative-event',
+    ephemeral: true,
+  });
+  return;
 };
+
+export const ChoseiHostForm: Form<keyof Inputs> = new Form<keyof Inputs>(
+  { route, handler },
+  {
+    title: 'Host new Event',
+    inputs: [
+      {
+        name: 'name',
+        customId: 'eventNameInput',
+        label: 'Name',
+        required: true,
+        style: TextInputStyle.Short,
+      },
+      {
+        name: 'detail',
+        customId: 'eventDetailInput',
+        label: 'Detail',
+        maxLength: 100,
+        required: false,
+        style: TextInputStyle.Paragraph,
+      },
+    ],
+  }
+);
--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,5 +1,6 @@
 import { Events, Interaction } from 'discord.js';
-import { ChoseiHostForm } from '~/commands/chosei/forms/host.form';
+import { Button } from '~/lib/button';
+import { Form } from '~/lib/form';
 import { routeParse } from '~/lib/route';
 import { BotCommand, BotEvent } from '~/types';
 
@@ -17,15 +18,30 @@ const event: BotEvent = {
     if (i.isButton()) {
       const route = routeParse(i.customId);
       const command: BotCommand = i.client.commands.get(route.commandName);
-      const button = command.buttons?.find((btn) => btn.route?.customId == route.customId);
+      const button = command.reactables?.find((btn) => btn.route?.customId == route.customId);
       if (!button || !button.handler) {
         console.error('nyaa');
+        return;
+      }
+      if (!(button instanceof Button)) {
+        console.error('nande button igai ga arundesuka');
         return;
       }
       await button.handler(i);
     }
     if (i.isModalSubmit()) {
-      await ChoseiHostForm.handler(i);
+      const route = routeParse(i.customId);
+      const command: BotCommand = i.client.commands.get(route.commandName);
+      const form = command.reactables?.find((form) => form.route?.customId == route.customId);
+      if (!form || !form.handler) {
+        console.error('is modal submit mya');
+        return;
+      }
+      if (!(form instanceof Form)) {
+        console.error('nande form igai ga arundesuka');
+        return;
+      }
+      await form.handler(i);
     }
     return;
   },
